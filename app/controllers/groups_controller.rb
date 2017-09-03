64e566025349d6a1efb7734dc45e8f955666aed2
Hayato oyanagi <oyana@oya.local>
2017-09-03 18:11:37 +0900

    PR3を再現

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,8 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[edit update]
   def index
-    @groups = current_user.groups
-    @user = current_user
+    @groups = Group.all
+    @group_id = params[:group_id]
   end
 
   def new
@@ -10,38 +10,34 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = current_user.groups.new(group_params)
-    if @group.save
-      redirect_to group_messages_path(@group), notice: "グループを作成しました"
+    group = Group.new(group_params)
+    if group.save
+      redirect_to root_path, notice: "グループを作成しました"
     else
       flash.now[:alert] = "グループ名を入力してください"
-      render :new
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: "グループを編集しました"
+      redirect_to root_path, notice: "グループを編集しました"
     else
       flash.update[:alert] = "グループ名を入力してください"
-      render :edit
+      render action: 'edit'
     end
   end
 
   private
 
   def group_params
-    params.require(:group).permit(:name, user_ids:[])
+    params.require(:group).permit(:name, :user_ids)
   end
 
   def set_group
     @group = Group.find(params[:id])
   end
-
 end
-
-
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,22 @@
-class UsersController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
-  before_action :check_group, only: [:edit]
 
-  def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    respond_to do |format|
-       format.html
-       format.json
-    end
-  end
+class UsersController < ApplicationController
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
-    @user.update(user_params)
-    if @user.update
-      redirect_to root_path
-     else
-      flash.now[:alert] = "名前を入力してください"
-      render :edit
-    end
+    user = User.find(params[:id])
+    user.update(user_params) if user.id == current_user.id
+    redirect_to root_path
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "名前を登録しました"
+      redirect_to root_path, notice: "グループを作成しました"
     else
-      flash.now[:alert] = "名前を入力してください"
-      render :new
+      flash.now[:alert] = "グループ名を入力してください"
     end
   end
 
@@ -38,15 +25,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
-  def set_group
-   @user = User.find(params[:id])
-  end
-
-  def check_group
-    redirect_to root_path unless @user.id == current_user.id
-  end
 end
-
-
-
